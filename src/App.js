f3d611ce3d3a357c15da0a8a65e197742b5ba890
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-02-16 14:47:43 +0100

    adding the functionality to delete button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { useEffect } from "react";
 import { useState } from "react";
 import "./App.css";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function App() {
   // const [columns, setColumns] = useState([]);
   const [records, setRecords] = useState([]);
+  const navigate = useNavigate()
 
   useEffect(() => {
     axios
@@ -17,14 +18,32 @@ function App() {
       .catch((err) => console.log(err));
   }, []);
 
+  const handleDelete = (id) => {
+    const confirmDelete = window.confirm("Do you want to delete");
+    if (confirmDelete) {
+      axios
+        .delete("http://localhost:3232/user/" + id)
+        .then((res) => {
+          // alert("Data deleted succesfuuly");
+          
+          window.location.reload()
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
   return (
     <div className="container mt-5">
       <div className="text-end round-4">
-        <Link to="/create" className="round-5 btn btn-sm btn-primary mt-1">ADD +</Link>
+        <Link to="/create" className="round-5 btn btn-sm btn-primary mt-1">
+          ADD +
+        </Link>
       </div>
       <table className="table">
         <thead>
-          <tr >
+          <tr>
             <th>ID</th>
             <th>NAME</th>
             <th>EMAIL</th>
@@ -39,8 +58,18 @@ function App() {
                 <td>{d.name}</td>
                 <td>{d.email}</td>
                 <td>
-                  <Link to={`/update/${d.id}`} className="btn btn-sm btn-success">UPDATE</Link>
-                  <Link to="/" className="btn ms-2 btn-sm btn-danger">DELETE</Link>
+                  <Link
+                    to={`/update/${d.id}`}
+                    className="btn btn-sm btn-success"
+                  >
+                    UPDATE
+                  </Link>
+                  <button
+                    onClick={(e) => handleDelete(d.id)}
+                    className="btn ms-2 btn-sm btn-danger"
+                  >
+                    DELETE
+                  </button>
                 </td>
               </tr>
             );
